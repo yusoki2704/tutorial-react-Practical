b26555a536bd81bb6fd629cbe79746f8987fc310
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-08-02 19:49:38 +0900

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/Todo.tsx
@@ -0,0 +1,11 @@
+type TodoType = {
+  userId: number;
+  title: string;
+  completed?: boolean;
+};
+
+export const Todo = (props: TodoType) => {
+  const { title, userId, completed = false } = props;
+  const completedMark = completed ? "[完]" : "[未]";
+  return <p>{`${completedMark} ${title}(ユーザー：${userId})`}</p>;
+};
